ed130e906ef15100a7a7cd2b7251b1a9e68deee8
Anthony George <ageorge530@gmail.com>
2020-06-17 02:07:59 +0000

    Automatically backed up by Learn

--- lib/custom_errors.rb
+++ lib/custom_errors.rb
@@ -1,2 +1,18 @@
+class Person
+  attr_accessor :partner, :name
 
+  def initialize(name)
+    @name = name
+  end
+
+  def get_married(person)
+    self.partner = person
+    person.partner = self
+  end
+
+end
+
+beyonce = Person.new("Beyonce")
+beyonce.get_married("Jay-Z")
+puts beyonce.name
 
